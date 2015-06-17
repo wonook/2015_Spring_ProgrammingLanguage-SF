9792f26739b5cf2c174f7e4b37cc26b7e299079c
Won Wook SONG <wsong0512@gmail.com>
2015-06-17 23:48:23 +0900

    done upto 5 for assgn12, doing 6

--- sf/Assignment12.v
+++ sf/Assignment12.v
@@ -139,10 +139,25 @@ Check soundness : forall t t' T,
         else if (pred x)=0 then 0
         else 1 + (halve (pred (pred x))))
 >>
+<<
+  halve =
+    fix
+      (\f:nat->nat
+        \x:nat.
+          if0 x then 0
+          else if0 (pred x) then 0
+          else (succ (f (pred (pred x))))).
+>>
 *)
 
 Definition halve : tm :=
-  FILL_IN_HERE.
+    tfix
+      (tabs Halve (TArrow TNat TNat)
+        (tabs X TNat
+          (tif0 (tvar X) (tnat 0)
+            (tif0 (tpred (tvar X)) (tnat 0)
+              (tsucc 
+                (tapp (tvar Halve) (tpred (tpred (tvar X))))))))).
 
 Example halve_type: empty |- halve \in TArrow TNat TNat.
 Proof.
@@ -159,14 +174,39 @@ Qed.
 Example halve_11: tapp halve (tnat 11) ==>* tnat 5.
 Proof.
   (* unfold halve; normalize. *)
-  unfold halve; normalize.
+  unfold halve. normalize.
 Qed.
 
 
 Theorem halve_correct: forall n,
    tapp halve (tnat (n+n)) ==>* tnat n.
 Proof.
-  exact FILL_IN_HERE.
+  unfold halve. intros. induction n.
+  - normalize.
+  - eapply multi_step. 
+      eapply ST_AppFix. apply v_abs. apply v_nat.
+    eapply multi_step.
+      eapply ST_App1. eapply ST_AppAbs. apply v_fix. apply v_abs.
+    eapply multi_step. simpl.
+      eapply ST_AppAbs. apply v_nat.
+    eapply multi_step. simpl.
+      eapply ST_If0Nonzero.
+    eapply multi_step.
+      eapply ST_If01. rewrite plus_comm. apply ST_PredNat.
+    eapply multi_step. simpl.
+      eapply ST_If0Nonzero. rewrite plus_comm. 
+    eapply multi_step. simpl.
+      eapply ST_Succ1. eapply ST_App2. apply v_fix. apply v_abs.
+      eapply ST_Pred. eapply ST_PredNat.
+    eapply multi_step. simpl.
+      eapply ST_Succ1. eapply ST_App2. apply v_fix. apply v_abs.
+      eapply ST_PredNat. simpl.
+    assert (forall A, A ==>* tnat n -> tsucc A ==>* tnat (S n)).
+      intros. eapply multi_trans.
+      assert (forall t t', t ==>* t' -> tsucc t ==>* tsucc t').
+        intros; induction H0; eauto.
+      apply H0. apply H. eauto.
+    apply H. apply IHn.
 Qed.
 
 (*-- Check --*)
@@ -187,7 +227,46 @@ Check halve_correct: forall n,
 Theorem tloop_diverges:
   ~ exists t, tapp tloop (tnat 0) ==>* t /\ normal_form step t.
 Proof.
-  exact FILL_IN_HERE.
+  unfold tloop, not, normal_form. 
+  assert (forall t:tm, tapp (tfix (tabs Loop (TArrow TNat TNat) (tabs X TNat (tapp (tvar Loop) (tvar X))))) (tnat 0) ==>* t \/ exists t':tm, t ==> t').
+  intros. 
+    remember (exists t':tm, t ==> t').
+    left.
+
+    eapply multi_step.
+      eapply ST_AppFix. apply v_abs. apply v_nat.
+    eapply multi_step.
+      eapply ST_App1. eapply ST_AppAbs. apply v_fix. apply v_abs. simpl.
+    eapply multi_step.
+      eapply ST_AppAbs. apply v_nat. simpl.
+
+    eapply multi_step.
+      eapply ST_AppFix. apply v_abs. apply v_nat.
+    eapply multi_step.
+      eapply ST_App1. eapply ST_AppAbs. apply v_fix. apply v_abs. simpl.
+    eapply multi_step.
+      eapply ST_AppAbs. apply v_nat. simpl.
+
+    eapply multi_step.
+      eapply ST_AppFix. apply v_abs. apply v_nat.
+    eapply multi_step.
+      eapply ST_App1. apply ST_AppAbs. apply v_fix. apply v_abs. simpl.
+    eapply multi_step.
+      eapply ST_AppAbs. apply v_nat. simpl.
+
+    eapply multi_step.
+      eapply ST_AppFix. apply v_abs. apply v_nat.
+    eapply multi_step.
+      eapply ST_App1. apply ST_AppAbs. apply v_fix. apply v_abs. simpl.
+    eapply multi_step.
+      eapply ST_AppAbs. apply v_nat. simpl.
+
+    inversion H; subst; clear H.
+    inversion H0; subst; clear H0.
+    apply H1.
+    inversion H; subst; eauto.
+    inversion H0; subst; eauto.
+    inversion H2; subst; eauto.
 Qed.
 
 (*-- Check --*)
--- sf/Assignment12_05.v
+++ sf/Assignment12_05.v
@@ -10,34 +10,74 @@ Require Export Assignment12_04.
         else if (pred x)=0 then 0
         else 1 + (halve (pred (pred x))))
 >>
+<<
+  halve =
+    fix
+      (\f:nat->nat
+        \x:nat.
+          if0 x then 0
+          else if0 (pred x) then 0
+          else (succ (f (pred (pred x))))).
+>>
 *)
 
 Definition halve : tm :=
-  FILL_IN_HERE.
+    tfix
+      (tabs Halve (TArrow TNat TNat)
+        (tabs X TNat
+          (tif0 (tvar X) (tnat 0)
+            (tif0 (tpred (tvar X)) (tnat 0)
+              (tsucc 
+                (tapp (tvar Halve) (tpred (tpred (tvar X))))))))).
 
 Example halve_type: empty |- halve \in TArrow TNat TNat.
 Proof.
   (* unfold halve; eauto 10. *)
-  exact FILL_IN_HERE.
+  unfold halve; eauto 10.
 Qed.
 
 Example halve_10: tapp halve (tnat 10) ==>* tnat 5.
 Proof.
   (* unfold halve; normalize. *)
-  exact FILL_IN_HERE.
+  unfold halve; normalize.
 Qed.
 
 Example halve_11: tapp halve (tnat 11) ==>* tnat 5.
 Proof.
   (* unfold halve; normalize. *)
-  exact FILL_IN_HERE.
+  unfold halve. normalize.
 Qed.
 
 
 Theorem halve_correct: forall n,
    tapp halve (tnat (n+n)) ==>* tnat n.
 Proof.
-  exact FILL_IN_HERE.
+  unfold halve. intros. induction n.
+  - normalize.
+  - eapply multi_step. 
+      eapply ST_AppFix. apply v_abs. apply v_nat.
+    eapply multi_step.
+      eapply ST_App1. eapply ST_AppAbs. apply v_fix. apply v_abs.
+    eapply multi_step. simpl.
+      eapply ST_AppAbs. apply v_nat.
+    eapply multi_step. simpl.
+      eapply ST_If0Nonzero.
+    eapply multi_step.
+      eapply ST_If01. rewrite plus_comm. apply ST_PredNat.
+    eapply multi_step. simpl.
+      eapply ST_If0Nonzero. rewrite plus_comm. 
+    eapply multi_step. simpl.
+      eapply ST_Succ1. eapply ST_App2. apply v_fix. apply v_abs.
+      eapply ST_Pred. eapply ST_PredNat.
+    eapply multi_step. simpl.
+      eapply ST_Succ1. eapply ST_App2. apply v_fix. apply v_abs.
+      eapply ST_PredNat. simpl.
+    assert (forall A, A ==>* tnat n -> tsucc A ==>* tnat (S n)).
+      intros. eapply multi_trans.
+      assert (forall t t', t ==>* t' -> tsucc t ==>* tsucc t').
+        intros; induction H0; eauto.
+      apply H0. apply H. eauto.
+    apply H. apply IHn.
 Qed.
 
 (*-- Check --*)
